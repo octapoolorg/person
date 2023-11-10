80b746b1f232c632753f8621466e4d047902fa34
Octa Pool <octapool@outlook.com>
2023-11-10 13:53:08 +0500

    before change

--- resources/views/names/show.blade.php
+++ resources/views/names/show.blade.php
@@ -231,31 +231,31 @@
             <section class="my-8">
                 <div class="flex flex-wrap justify-center md:justify-start gap-2 sm:gap-4">
                     <!-- Twitter -->
-                    <a href="https://twitter.com/intent/tweet?text=url-current-page"
+                    <a href="https://twitter.com/intent/tweet?text={!! request()->url() !!}"
                         class="px-3 py-2 sm:px-4 sm:py-2 bg-gray-900 text-white rounded-full hover:bg-gray-950"
                         target="_blank" rel="nofollow">
-                        <i class="fab fa-twitter"></i> Share
+                        <i class="fab fa-x-twitter"></i> Share
                     </a>
                     <!-- Facebook -->
-                    <a href="https://www.facebook.com/sharer/sharer.php?u=url-current-page"
+                    <a href="https://www.facebook.com/sharer/sharer.php?u={!! request()->url() !!}"
                         class="px-3 py-2 sm:px-4 sm:py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600"
                         target="_blank" rel="nofollow">
                         <i class="fab fa-facebook-f"></i> Share
                     </a>
                     <!-- LinkedIn -->
-                    <a href="https://www.linkedin.com/cws/share?url=url-current-page"
+                    <a href="https://www.linkedin.com/cws/share?url={!! request()->url() !!}"
                         class="px-3 py-2 sm:px-4 sm:py-2 bg-blue-700 text-white rounded-full hover:bg-blue-800"
                         target="_blank" rel="nofollow">
                         <i class="fab fa-linkedin-in"></i> Share
                     </a>
                     <!-- Reddit -->
-                    <a href="http://www.reddit.com/submit?url=url-current-page"
+                    <a href="http://www.reddit.com/submit?url={!! request()->url() !!}"
                         class="px-3 py-2 sm:px-4 sm:py-2 bg-red-500 text-white rounded-full hover:bg-red-600"
                         target="_blank" rel="nofollow">
                         <i class="fab fa-reddit-alien"></i> Share
                     </a>
                     <!-- Mail -->
-                    <a href="mailto:?subject={!! $data['nameDetails']->name !!} name - all you need to know&amp;body=url-current-page"
+                    <a href="mailto:?subject={!! $data['nameDetails']->name !!} name details - all you need to know&amp;body={!! request()->url() !!}"
                         class="px-3 py-2 sm:px-4 sm:py-2 bg-gray-500 text-white rounded-full hover:bg-gray-600"
                         target="_blank" rel="nofollow" title="Share via email">
                         <i class="fas fa-envelope"></i> Share
