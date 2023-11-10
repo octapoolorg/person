383e9c73fd742463a028cd6b465c5da99323e7eb
Octa Pool <octapool@outlook.com>
2023-11-10 17:39:05 +0500

    updated ui

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[HomeController::class,'index'])->name('home');
 
 Route::get('names', [NameController::class, 'index'])->name('names.index');
-Route::get('names/{gender}',[NameController::class,'genderNames'])->name('names.gender');
+Route::get('names/search', [NameController::class, 'search'])->name('names.search');
 Route::get('/random-names', [NameController::class, 'getRandomNames'])->name('names.random');
 
 Route::get('name/{name}', [NameController::class, 'show'])->name('names.show');
-Route::get('names/search/', [NameController::class, 'search'])->name('names.search');
-
+Route::get('names/{gender}',[NameController::class,'genderNames'])->name('names.gender');
 
 Route::get('blog', [BlogController::class, 'index'])->name('blog.index');
 Route::get('blog/{slug}', [BlogController::class, 'show'])->name('blog.show');
