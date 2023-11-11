b89a69e144184f45ce6fa2a91bc53ac6cfaefc5e
Octa Pool <octapool@outlook.com>
2023-11-11 17:48:56 +0500

    update seo

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\View\View;
 use Wink\WinkPage;
+use Artesaos\SEOTools\Facades\SEOTools;
 
 class PageController extends Controller
 {
     public function show($page): View
     {
         $page = WinkPage::where('slug', $page)->firstOrFail();
+        $this->seoTags($page);
         return view('pages.show', compact('page'));
+    }
+
+    public function seoTags($page)
+    {
+        SEOTools::setTitle($page->title);
+        SEOTools::setDescription($page->excerpt);
     }
 }
