2a662b39f8a843805b5ed60314775e656426e870
Octa Pool <octapool@outlook.com>
2023-11-14 15:32:28 +0500

    calc

---
+++ app/Http/Controllers/Tools/NumerologyController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Tools;
+
+use App\Http\Controllers\Controller;
+use App\Services\Numerology\NumerologyFactory;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class NumerologyController extends Controller
+{
+    public function calculator(): View
+    {
+        return view('tools.numerology.calculator');
+    }
+
+    public function calculate(): View
+    {
+        $name = request('name');
+        $dob = request('dob');
+
+        NumerologyFactory::create('pythagorean')->getNumerologyData($name, $dob);
+
+        return view('tools.numerology.calculator');
+    }
+}
