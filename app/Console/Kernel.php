43fe491248119376dd339b278b502036227aa3c5
Octa Pool <octapool@outlook.com>
2023-12-17 13:27:24 +0500

    updated todo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('app:activate-names')->daily();
+        $schedule->command('app:seo:sitemap')->daily();
     }
 
     /**
