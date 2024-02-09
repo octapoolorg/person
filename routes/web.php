75c94c4ab64a7db4ac438deb4a04f0bab03da99d
Octa Pool <octapool@outlook.com>
2024-02-09 21:35:21 +0500

    Refactor routes in web.php file

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,10 @@ Route::get('names/origin/{origin}',[NameController::class,'origin'])->name('name
 Route::get('names/category/{category}',[NameController::class,'category'])->name('names.category');
 Route::get('names/starting/{letter}',[NameController::class,'starting'])->name('names.starting');
 
-Route::get('/static/images/wallpaper/name/funky/{name}.jpg', [NameController::class, 'wallpaper'])->name('names.wallpaper');
-Route::get('/static/images/signature/style/{font}/{name}.jpg', [NameController::class, 'signature'])->name('names.signature');
+Route::get('static/images/wallpaper/name/funky/{name}.jpg', [NameController::class, 'wallpaper'])->name('names.wallpaper');
+Route::get('static/images/signature/style/{font}/{name}.jpg', [NameController::class, 'signature'])->name('names.signature');
 
-Route::post('/names/{name}/comments', [CommentController::class, 'store'])->name('names.comments.store');
+Route::post('names/{name}/comments', [CommentController::class, 'store'])->name('names.comments.store');
 
 Route::get('tools/numerology/calculator', [NumerologyController::class, 'calculator'])->name('tools.numerology.calculator.get');
 Route::post('tools/numerology/calculator', [NumerologyController::class, 'calculate'])->name('tools.numerology.calculate.post');
@@ -38,11 +38,11 @@ Route::get('pages/{page}', [PageController::class, 'show']);
 
 
 /* Legacy Routes - Redirects */
-Route::get('/static/images/signature/{name}/{font}.jpg', function ($name, $font) {
+Route::get('static/images/signature/{name}/{font}.jpg', function ($name, $font) {
     return redirect()->route('names.signature', [$font,$name], 301);
 });
 
-Route::get('/static/images/name/{name}.jpg', function ($name) {
+Route::get('static/images/name/{name}.jpg', function ($name) {
     return redirect()->route('names.wallpaper', [$name], 301);
 });
 
@@ -50,8 +50,3 @@ Route::get('/static/images/name/{name}.jpg', function ($name) {
 Route::get('sitemap.xml', function () {
     return redirect('sitemap_index.xml', 301);
 });
-
-
-Route::get('{url}/', function ($url) {
-    return redirect($url, 301);
-})->where('url', '.*');
