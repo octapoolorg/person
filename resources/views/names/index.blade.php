834637c2f58460eaf2d9c9b8db662b2ed43a6c52
Octa Pool <octapool@outlook.com>
2024-02-12 03:28:01 +0500

    Update name index.blade.php layout

--- resources/views/names/index.blade.php
+++ resources/views/names/index.blade.php
@@ -4,16 +4,15 @@
     <section class="max-w-7xl mx-auto px-6 md:px-4 lg:px-8">
         <section class="flex flex-col lg:flex-row mb-12 mt-8 md:mt-20">
             <main class="w-full lg:w-2/3 px-4 mb-4 lg:mb-0">
-                <div id="search-results" class="space-y-4">
+                <div id="search-results" class="space-y-4 lg:space-y-6">
                     @forelse ($names as $name)
-                        <a href="{!! route('names.show', $name) !!}" class="p-4 shadow dark:shadow-none rounded-lg flex justify-start items-center dark:border dark:border-base-700 hover:bg-base-50 dark:hover:bg-base-800 transition duration-300 ease-in-out">
-                            <div class="flex-grow">
+                        <a href="{!! route('names.show', $name) !!}" class="p-4 shadow dark:shadow-none rounded-lg flex justify-start items-center dark:border dark:border-base-700 bg-surface hover:bg-base-50 dark:hover:bg-base-800 transition duration-300 ease-in-out">
+                            <div class="w-full flex-grow">
                                 <h2 class="text-xl font-semibold text-primary-800 dark:text-primary-100 capitalize">
                                     {{ $name->name }} ({{ $name->gender->name[0] }})
                                 </h2>
-                                <p class="text-md text-base-500 dark:text-base-300">{{ $name->meaning }}</p>
+                                <p class="text-md text-base-500 dark:text-base-300 over truncate pe-10">{{ $name->meaning }}</p>
                             </div>
-                            <span class="text-primary-600 hover:text-primary-900 dark:text-primary-400 dark:hover:text-primary-200 transition duration-300 ease-in-out">Learn more</span>
                         </a>
                     @empty
                         <div class="text-center py-8">
