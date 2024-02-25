b901cd7f1ed49847ea98b7d63e1b669edc3854d7
Octa Pool <octapool@outlook.com>
2024-02-25 05:50:33 +0500

    Refactor redirects and add new route for category

--- routes/redirects.php
+++ routes/redirects.php
@@ -3,23 +3,22 @@
 use App\Models\Redirect;
 use Illuminate\Support\Facades\Route;
 
-// $cacheKey = 'redirects';
+$cacheKey = 'redirects';
 
-// $redirects = cache_remember($cacheKey, function () {
-//     return Redirect::all()->pluck('target', 'source');
-// });
+$redirects = cache_remember($cacheKey, function () {
+    return Redirect::all()->pluck('target', 'source');
+});
 
-// foreach ($redirects as $source => $target) {
-//     Route::redirect($source, $target, 301);
-// }
-
+foreach ($redirects as $source => $target) {
+    Route::redirect($source, $target, 301);
+}
 
 Route::redirect('names', 'names/search', 301);
 
-// Route::get('/{any}-1', function ($any, \Illuminate\Http\Request $request) {
-//     // Remove the -1 and preserve query parameters
-//     return redirect("/$any" .'?'. $request->getQueryString(), 301);
-// })->where('any', '.*');
+// Remove the -1 from the URL
+Route::get('/name/{any}-1', function ($any) {
+    return redirect("name/$any" , 301);
+})->where('any', '.*');
 
 Route::get('static/images/signature/style/monsieur-la-doulaise/{name}.jpg', function ($name) {
     return redirect()->route('names.signature', ['cursive', $name], 301);
@@ -30,13 +29,18 @@ Route::get('static/images/signature/{name}/{font}.jpg', function ($name, $font) 
     return redirect()->route('names.signature', [$font, $name], 301);
 });
 
-Route::get('static/images/name/{name}.jpg', function ($name) {
+Route::get('/static/images/name/{name}.jpg', function ($name) {
     return redirect()->route('names.wallpaper', [$name], 301);
 });
 
 Route::get('/names/{gender}', function (string $gender) {
     return redirect()->route('names.search', ['gender' => $gender], 301);
 })->whereIn('gender', ['masculine', 'feminine', 'unisex']);
+
+
+Route::get('/category/{category}', function (string $category) {
+    return redirect()->route('names.search', ['category' => $category], 301);
+})->where('category', '.*');
 
 Route::get('sitemap.xml', function () {
     return redirect('sitemap_index.xml', 301);
