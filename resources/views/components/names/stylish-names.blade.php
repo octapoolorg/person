8d6fe4845591ddac74a13bba376ebeac1eb0356e
Octa Pool <octapool@outlook.com>
2024-02-29 10:54:16 +0500

    Refactor code for fancy-texts and usernames components

--- resources/views/components/names/stylish-names.blade.php
+++ resources/views/components/names/stylish-names.blade.php
@@ -19,7 +19,7 @@
     <div class="overflow-x-auto" x-data>
         <ul class="divide-y divide-base-200 dark:divide-base-700 overflow-x-auto" id="fancy-texts">
             @foreach ($name->fancyTexts as $fancyText)
-                <li @click.prevent="Utility.copyTextToClipboard($event.target.innerText,$event)"
+                <li
                     class="text-lg p-4 hover:bg-base-100 dark:hover:bg-base-700 transition ease-in-out duration-150 cursor-pointer copy-to-clipboard text-base-900 dark:text-base-100 whitespace-nowrap select-all"
                     aria-label="Select {!! $fancyText !!} style">
                     {!! $fancyText !!}
