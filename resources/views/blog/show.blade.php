a7c3f124ee4b36afe257e8dd1e570e1f8b61923d
Octa Pool <octapool@outlook.com>
2024-02-29 08:40:00 +0500

    Update blog show.blade.php layout

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -3,14 +3,14 @@
 @section('content')
     <section class="max-w-7xl mx-auto px-6 md:px-4 lg:px-8">
         <section class="container mb-12 pt-8">
-            <main class="p-4 md:ps-0 mb-4 lg:mb-0 bg-surface dark:bg-base-800">
+            <main class="p-4 md:ps-0 mb-4 lg:mb-0 bg-surface dark:bg-base-800 shadow rounded-lg">
                 <article class="md:p-10 prose prose-base max-w-none dark:prose-invert">
                     @if (!empty($post->featured_image) && file_exists(public_path($post->featured_image)))
                         <img src="{{ $post->featured_image }}" alt="{{ $post->title }}"
                             class="w-full rounded-t-lg mb-6 object-cover">
                     @endif
                     <h1 class="text-3xl lg:text-5xl font-semibold text-base-800 dark:text-base-100 mb-6">{{ $post->title }}</h1>
-                    <div class="flex items-center text-base-600 dark:text-base-300 mb-6">
+                    <div class="flex flex-col md:flex-row items-center text-base-600 dark:text-base-300 mb-6">
                         @if (!empty($post->author->avatar) && file_exists(public_path($post->author->avatar)))
                             <img src="{{ $post->author->avatar }}" alt="author {{ $post->author->name }}"
                                 class="h-12 w-12 rounded-full mr-4">
