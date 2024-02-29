925550120ae26b2c8134e55fffbae6e2ef4c1253
Octa Pool <octapool@outlook.com>
2024-02-29 10:58:51 +0500

    Update CSS classes for select text functionality

--- resources/views/components/names/api/fancy-texts.blade.php
+++ resources/views/components/names/api/fancy-texts.blade.php
@@ -1,5 +1,5 @@
 @foreach ($fancyTexts as $fancyText)
-<li class="text-lg p-4 hover:bg-base-100 dark:hover:bg-base-700 transition ease-in-out duration-150 cursor-pointer copy-to-clipboard text-base-900 dark:text-base-100 whitespace-nowrap select-all"
+<li class="text-lg p-4 hover:bg-base-100 dark:hover:bg-base-700 transition ease-in-out duration-150 cursor-pointer copy-to-clipboard text-base-900 dark:text-base-100 whitespace-nowrap select-text"
     aria-label="Select {!! $fancyText !!} style">
     {!! $fancyText !!}
 </li>
