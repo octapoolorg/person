caa29e04e6b8c2e7a7d1e0132e4aec6cf6f9e6bc
Octa Pool <octapool@outlook.com>
2024-03-07 02:07:02 +0500

    Add Google AdSense code to head.blade.php and comment out ads script in show.blade.php

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -9,3 +9,9 @@
     });
 </script>
 @stack('head_tags')
+
+@production
+<meta name="google-adsense-account" content="ca-pub-5845517826660589">
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5845517826660589"
+crossorigin="anonymous"></script>
+@endproduction
