0e56d1b52b645645783825ec01dfbb27fcf6dba0
Octa Pool <octapool@outlook.com>
2024-03-07 01:57:22 +0500

    Add async attribute to Pinterest script

--- resources/views/names/show.blade.php
+++ resources/views/names/show.blade.php
@@ -75,6 +75,7 @@
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5845517826660589"
     crossorigin="anonymous"></script>
     @endPushOnce
+
     @pushonce('scripts')
         <script type="text/javascript" async defer data-pin-hover="true"
                 src="//assets.pinterest.com/js/pinit.js"></script>
